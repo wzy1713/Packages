444a84cc3072796adb9b5435115fc5103cc7e885
wzy1713 <wzy1713@126.com>
2018-06-08 10:23:42 +0800

    add download data from URL address
    add fetch_housing_data function

--- Functions.py
+++ Functions.py
@@ -281,7 +281,15 @@ def makdir(proj_path, batpath):
     p.wait()
     print(p.returncode)
 
-
+#Download data file from URL and unzip it
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
 
 
 
